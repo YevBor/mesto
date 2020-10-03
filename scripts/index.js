0b94cd20bb3cf87dfa32a7b66d64975e672d9909
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2020-10-03 18:39:39 +0300

    last changes

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ let buttonOpenPopup = document.querySelector(".profile__edit-button");
 let buttonClosePopup = document.querySelector(".popup__close-image");
 
 let popup = document.querySelector(".popup");
+let close = document.querySelector(".popup_closed");
 
 let nameInput = document.querySelector(".popup__input-name");
 let jobInput = document.querySelector(".popup__input-profession");
@@ -9,33 +10,28 @@ let jobInput = document.querySelector(".popup__input-profession");
 let changeInputName= document.querySelector(".profile__title");
 let changeInputJob = document.querySelector(".profile__subtitle");
 
-let state = 0;
-
 let formElement = document.querySelector(".popup__container");
 
-function popupToggle(){
-        popup.classList.toggle("popup_closed");
-}
-
-function openWindowChecker(){
-    if ( state === 0){
-        state = 1;
-        popupToggle();
-    }else{
-        nameInput.value = "";
-        jobInput.value = ""; 
-        state = 0;
-        popupToggle();    
-    }
+function togglePopup(){
+        if (popup.classList.contains("popup_closed")){
+            console.log("nuka");
+            nameInput.value = changeInputName.textContent
+            jobInput.value = changeInputJob.textContent
+        }else{   
+            console.log("naka");
+        }
+        popup.classList.toggle("popup_closed");        
 }
 
 function formSubmitHandler (evt) {
-    evt.preventDefault();
-    changeInputName.textContent = nameInput.value;
-    changeInputJob.textContent = jobInput.value;
-    openWindowChecker();
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    changeInputName.textContent = nameInput.value
+    changeInputJob.textContent = jobInput.value
+    togglePopup()
 }
 
-buttonOpenPopup.addEventListener("click", openWindowChecker);
-buttonClosePopup.addEventListener("click", openWindowChecker);
+formElement.addEventListener('submit', formSubmitHandler); 
+
+buttonOpenPopup.addEventListener("click", togglePopup);
+buttonClosePopup.addEventListener("click", togglePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
