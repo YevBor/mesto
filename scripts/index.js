7e3b50f68e8a87dbfc3a3f57d80377acc9d7488b
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2020-10-19 13:08:42 +0300

    almost finished add new card function

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-
+const template = document.querySelector(".template");
 //popups open buttons
 let buttonOpenPopupEdit = document.querySelector(".profile__edit-button");
 let buttonOpenPopupNewCard = document.querySelector(".profile__add-button");
@@ -8,18 +8,23 @@ let buttonClosePopupNewCard = document.querySelector(".popup_type_new-card .popu
 
 
 
+
 //popups
 let popupEdit = document.querySelector(".popup_type_edit");
 let popupNewCard = document.querySelector(".popup_type_new-card")
 
 
-let nameInput = document.querySelector(".popup_type_edit .popup__input-name");
-let jobInput = document.querySelector(".popup_type_edit .popup__input-profession");
+const nameInput = document.querySelector(".popup_type_edit .popup__input-name");
+const jobInput = document.querySelector(".popup_type_edit .popup__input-profession");
+
+const cardName = document.querySelector(".popup_type_new-card .popup__input-name");
+const cardUrl = document.querySelector(".popup_type_new-card .popup__input-profession");
 
 let changeInputName= document.querySelector(".profile__title");
 let changeInputJob = document.querySelector(".profile__subtitle");
 
 let formElement = document.querySelector(".popup_type_edit .popup__container");
+let formElementCards = document.querySelector(".popup_type_new-card .popup__container");
 
 function togglePopup(popup){
         if (popup.classList.contains("popup_closed") && popup.classList.contains("popup_type_edit")){
@@ -38,7 +43,15 @@ function formSubmitHandler (evt) {
     togglePopup(popupEdit);
 }
 
+function formSubmitCards (evt) {
+    evt.preventDefault();
+    console.log(evt);
+    console.log(cardName.value);
+    console.log(cardUrl.value);
+};
+
 formElement.addEventListener('submit', formSubmitHandler); 
+formElementCards.addEventListener('submit', formSubmitCards); 
 
 buttonOpenPopupEdit.addEventListener("click", function () { 
     togglePopup(popupEdit)
@@ -83,20 +96,46 @@ const initialCards = [
 ]; 
 
 const cards = document.querySelector('.cards');
+//cards
+let cardImport = document.querySelector(".cards__item");
 
 const addButton = document.querySelector('.profile__add-button');
 
 const renderList = () =>{
-    const items = initialCards.map(element => {
-        return `<div class="cards__item">
-        <img class="cards__image " src="${element.link}" alt="${element.name}">
-        <div class="cards__rectangle">
-            <h2 class="cards__title">${element.name}</h2>
-            <button type="button"  aria-label="кнопка лайк" class="cards__like-button pointer-opacity"></button>
-        </div>
-    </div>`
-    }).join('');
-    cards.insertAdjacentHTML('afterbegin', items);
+    const items = initialCards.map(element => getItems(element));
+    cards.prepend(...items);
+    console.log(cards);
+}
+
+const getItems = (element) => {
+    const card = template.content.cloneNode(true);
+    card.querySelector(".cards__image").src=element.link
+    card.querySelector(".cards__image").alt=element.name
+    card.querySelector(".cards__title").innerText=element.name
+    console.log(card);
+    return card;
+};
+    // `<div class="cards__item">
+    //     <img class="cards__image " src="${element.link}" alt="${element.name}">
+    //     <div class="cards__rectangle">
+    //         <h2 class="cards__title">${element.name}</h2>
+    //         <button type="button"  aria-label="кнопка лайк" class="cards__like-button pointer-opacity"></button>
+    //     </div>
+    // </div>`
+// );
+
+
+const bindHandlers = () => {
+    addButton.addEventListener("click", () => {
+        const item = getItems({
+            name: cardName.value,
+            link: cardUrl.value
+        })
+        cards.prepend(item)
+        input.value = "";
+    });
+
 }
 
 renderList()
+bindHandlers()
